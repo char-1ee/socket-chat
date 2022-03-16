a620cfd2530f2c8a35734a6a4b1eab3611bc13ac
char-1ee <xingjianli59@gmail.com>
2022-03-16 13:56:26 +0800

    Add client

---
+++ Client.java
@@ -0,0 +1,48 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    private Socket socket;
+    private BufferedInputStream input;
+    private BufferedOutputStream output;
+
+    public Client(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+            input = new BufferedInputStream(System.in);
+            output = new BufferedOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException i) {
+            i.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String line = "";
+        while (!line.equals("exit")) {
+            try {
+                line = input.readLine();
+                output.write(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
